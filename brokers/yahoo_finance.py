01c7fb007094387012031b619ab0eea219638db3
anx1v <carlos.ariel.mamani@gmail.com>
2023-02-22 21:27:34 -0600

    Added requirements.txt Added custom exceptions

--- brokers/yahoo_finance.py
+++ brokers/yahoo_finance.py
@@ -1,4 +1,17 @@
 from base import BrokerAPI
+from exceptions import NotSupportedAction
 
 class YahooFinanceClient(BrokerAPI):
-    pass
+    """This class is only for data extraction from Yahoo Finance"""
+    def get_data(self):
+        pass
+
+    def get_api_secret(self):
+        pass
+
+    def open_position(self):
+        raise NotSupportedAction("Yahoo Finance does not support this action")
+
+    def close_position(self):
+        raise NotSupportedAction("Yahoo Finance does not support this action")
+
